cfd397916b23fe00be4f400ee14e9396dc5cb80e
Artem <a.danylov39@gmail.com>
2024-03-22 10:24:02 +0200

    new files: cdebug_last.h | def: funcs for getting last saved data

---
+++ src/cdebug_last.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef CDEBUG_LAST_H
+#define CDEBUG_LAST_H
+
+#include <CDebug/cdebug_args_t.h>
+
+static cdebug_args_t cdebug_last_info       = {"\0", "main", 0, "no info"};
+static cdebug_args_t cdebug_last_warning    = {"\0", "main", 0, "no warnings"};
+static cdebug_args_t cdebug_last_error      = {"\0", "main", 0, "no errors"};
+
+#endif //CDEBUG_LAST_H
